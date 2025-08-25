84e03633897f42ca155b8ce04d6e2bdd00144ec3
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-25 10:45:04 +0200

    Implement Error component with error handling and styled layout

--- src/pages/Error.jsx
+++ src/pages/Error.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import { useRouteError, Link } from "react-router-dom";
+import img from "../assets/not-found.svg";
+import Wrapper from "../assets/wrappers/ErrorPage";
 const Error = () => {
-  return <div>Error</div>;
+  const error = useRouteError();
+
+  console.log(error);
+  if (error.status === 404) {
+    return (
+      <Wrapper>
+        <div>
+          <img src={img} alt="not-found-image" />
+          <h3>Oops!</h3>
+          <p>We can't seem to find the page you're looking for</p>
+          <Link to={"/"}>back home</Link>
+        </div>
+      </Wrapper>
+    );
+  }
+
+  return <Wrapper>Something went wrong</Wrapper>;
 };
 
 export default Error;
