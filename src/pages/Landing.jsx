22b39cfa9a933f7f6909a58ab1ef8c06842d4032
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-25 11:51:24 +0200

    Implement loader function in Landing component to fetch cocktail data

--- src/pages/Landing.jsx
+++ src/pages/Landing.jsx
@@ -1,6 +1,19 @@
 import React from "react";
+import { useLoaderData } from "react-router-dom";
+import axios from "axios";
+
+const cocktailSearchUrl =
+  "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
+// eslint-disable-next-line react-refresh/only-export-components
+export const loader = async () => {
+  const searchTerm = "vodka";
+  const resp = await axios.get(`${cocktailSearchUrl}${searchTerm}`);
+  return { drinks: resp.data.drinks, searchTerm };
+};
 
 const Landing = () => {
+  const { drinks, searchTerm } = useLoaderData();
+  console.log(drinks);
   return <div>Landing</div>;
 };
 
