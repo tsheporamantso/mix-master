ce29e54a031ce6052e2e261c6faec3ac83d2b474
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-25 11:51:30 +0200

    Export SinglePageError component from pages index

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,3 +4,4 @@ export { default as Cocktail } from "./Cocktail.jsx";
 export { default as Error } from "./Error.jsx";
 export { default as HomeLayout } from "./HomeLayout.jsx";
 export { default as NewsLetter } from "./NewsLetter.jsx";
+export { default as SinglePageError } from "../components/SinglePageError.jsx";
