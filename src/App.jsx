b0cd2713747f6f7504222c29063a058a87e6d175
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-25 08:56:09 +0200

    Add additional routes and components for improved navigation

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,22 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  About,
+  Cocktail,
+  Error,
+  HomeLayout,
+  Landing,
+  NewsLetter,
+} from "./pages/index";
 
 function App() {
   const route = createBrowserRouter([
     {
       path: "/",
       element: <h2>Home page</h2>,
+    },
+    {
+      path: "/about",
+      element: <About />,
     },
   ]);
   return (
