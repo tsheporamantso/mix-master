db542a7192209a819529126bcdefa22174a6cc79
tsheporamantso <tshepo.ramantso@outlook.com>
2025-08-25 13:44:05 +0200

    Correct the cocktail path and add id

--- src/App.jsx
+++ src/App.jsx
@@ -25,7 +25,7 @@ function App() {
           errorElement: <SinglePageError />,
         },
         {
-          path: "cocktail",
+          path: "cocktail/:id",
           element: <Cocktail />,
         },
         {
